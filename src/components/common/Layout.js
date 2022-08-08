a26eb3339416669ff9c5d6161e61132a279e31fa
tubi55 <tubi55@nate.com>
2022-08-08 15:05:50 +0900

    서브레이아웃이 마운트 될때마다( 서브 컴포넌트가 변경될때마다 ) 해당 서브 컴포넌트에 on클래스 추가

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,6 +1,16 @@
+import { useEffect, useRef } from 'react';
+
 function Layout({ children, name }) {
+	//공통 레이아웃 프레임인 section을 담을 참조객체 생성
+	const frame = useRef(null);
+
+	//해당 컴포넌트가 마운트 되자마자 참조객체에 담겨있는 section요소에 on클래스 추가
+	useEffect(() => {
+		frame.current.classList.add('on');
+	}, []);
+
 	return (
-		<section className={`content ${name}`}>
+		<section className={`content ${name}`} ref={frame}>
 			<figure>
 				<img
 					src={`${process.env.PUBLIC_URL}/img/${name}.jpg`}
