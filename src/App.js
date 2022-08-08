627ca286d6dcddfcabb4570cbf9a0052ddea5e4f
tubi55 <tubi55@nate.com>
2022-08-08 10:11:48 +0900

    기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
+import { Route } from 'react-router-dom';
+
+//common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+//main
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+
+//sub
+import Department from './components/sub/Department';
+import Community from './components/sub/Community';
+import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/department'>
+				<Department />
+			</Route>
+
+			<Route path='/community'>
+				<Community />
+			</Route>
+
+			<Route path='/gallery'>
+				<Gallery />
+			</Route>
+
+			<Route path='/youtube'>
+				<Youtube />
+			</Route>
+
+			<Route path='/location'>
+				<Location />
+			</Route>
+
+			<Route path='/members'>
+				<Members />
+			</Route>
+
+			<Footer />
+		</>
 	);
 }
 
