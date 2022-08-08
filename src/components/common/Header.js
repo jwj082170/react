3994e9b247c6e6366809ca5c3640b6b7f138aa0a
tubi55 <tubi55@nate.com>
2022-08-08 10:50:00 +0900

    header에 라우터 링크 연결

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,67 @@
+import { Link, NavLink } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 function Header() {
-	return <div>Header</div>;
+	const active = { color: 'aqua' };
+
+	return (
+		<header>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<nav>
+				<ul id='gnb'>
+					<li>
+						<NavLink to='/department' activeStyle={active}>
+							DEPARTMENT
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeStyle={active}>
+							COMMUNITY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeStyle={active}>
+							GALLERY
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeStyle={active}>
+							YOUTUBE
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/location' activeStyle={active}>
+							LOCATION
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/members' activeStyle={active}>
+							MEMBERS
+						</NavLink>
+					</li>
+				</ul>
+
+				<ul className='util'>
+					<li>
+						<FontAwesomeIcon icon={faTwitter} />
+					</li>
+					<li>
+						<FontAwesomeIcon icon={faYoutube} />
+					</li>
+					<li>
+						<FontAwesomeIcon icon={faInstagram} />
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
