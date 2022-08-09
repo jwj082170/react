f19371f8c6ad7d6b7353e9f316c752801b8c72d6
tubi55 <tubi55@nate.com>
2022-08-09 17:11:13 +0900

    svg호버 모션 처리- 리액트버전

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -6,10 +6,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Youtube() {
-	const line = useRef(null);
+	const wrapper = useRef(null);
+	const icon = useRef(null);
 	const [Vids, setVids] = useState([]);
 	const [Open, setOpen] = useState(false);
 	const [Index, setIndex] = useState(0);
+	const [Len, setLen] = useState(null);
 
 	useEffect(() => {
 		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
@@ -17,54 +19,79 @@ function Youtube() {
 		const num = 6;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
 		axios.get(url).then((json) => {
-			console.log(json.data.items);
 			setVids(json.data.items);
 		});
 	}, []);
 
 	useEffect(() => {
-		console.log(line.current);
-	}, [line]);
+		if (!icon.current) return;
+		const svgItem = icon.current.querySelector('path');
+		const svgLen = svgItem.getTotalLength();
+		setLen(svgLen);
+
+		const pathAll =
+			wrapper.current.querySelectorAll('path');
+
+		pathAll.forEach((path) => {
+			path.style.strokeDasharray = svgLen;
+			path.style.strokeDashoffset = svgLen;
+		});
+	}, [Vids]);
 
 	return (
 		<>
 			<Layout name={'Youtube'}>
-				{Vids.map((vid, idx) => (
-					<article key={vid.id}>
-						<h2>
-							{vid.snippet.title.length > 30
-								? vid.snippet.title.substr(0, 30) + '...'
-								: vid.snippet.title}
-						</h2>
+				<div className='wrapper' ref={wrapper}>
+					{Vids.map((vid, idx) => (
+						<article key={vid.id}>
+							<h2>
+								{vid.snippet.title.length > 30
+									? vid.snippet.title.substr(0, 30) + '...'
+									: vid.snippet.title}
+							</h2>
 
-						<div className='txt'>
-							<p>
-								{vid.snippet.description.length > 200
-									? vid.snippet.description.substr(0, 200) +
-									  '...'
-									: vid.snippet.description}
-							</p>
-							<span>
-								{vid.snippet.publishedAt.split('T')[0]}
-							</span>
-						</div>
+							<div className='txt'>
+								<p>
+									{vid.snippet.description.length > 200
+										? vid.snippet.description.substr(
+												0,
+												200
+										  ) + '...'
+										: vid.snippet.description}
+								</p>
+								<span>
+									{vid.snippet.publishedAt.split('T')[0]}
+								</span>
+							</div>
 
-						<div className='pic'>
-							<img
-								src={vid.snippet.thumbnails.standard.url}
-								alt={vid.title}
-							/>
-							<FontAwesomeIcon
-								icon={faYoutube}
-								ref={line}
-								onClick={() => {
-									setOpen(true);
-									setIndex(idx);
+							<div
+								className='pic'
+								onMouseEnter={(e) => {
+									e.currentTarget.querySelector(
+										'path'
+									).style.strokeDashoffset = 0;
 								}}
-							/>
-						</div>
-					</article>
-				))}
+								onMouseLeave={(e) => {
+									e.currentTarget.querySelector(
+										'path'
+									).style.strokeDashoffset = Len;
+								}}>
+								<img
+									src={vid.snippet.thumbnails.standard.url}
+									alt={vid.title}
+								/>
+								<FontAwesomeIcon
+									ref={icon}
+									icon={faYoutube}
+									onClick={() => {
+										setOpen(true);
+										setIndex(idx);
+									}}
+								/>
+							</div>
+						</article>
+					))}
+				</div>
 			</Layout>
 
 			{Open && (
