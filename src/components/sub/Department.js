9e8eab40721238f6ce68ea5135e702b914fb2cef
tubi55 <tubi55@nate.com>
2022-08-09 10:08:13 +0900

    axios를 이용해서 members.json데이터 불러와서 state에 옮겨담기

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,11 +1,22 @@
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Department() {
-	return (
-		<Layout name={'Department'}>
-			<p>Department</p>
-		</Layout>
-	);
+	const [Members, setMembers] = useState([]);
+	useEffect(() => {
+		axios
+			.get(process.env.PUBLIC_URL + '/DB/members.json')
+			.then((json) => {
+				setMembers(json.data.members);
+			});
+	}, []);
+
+	useEffect(() => {
+		console.log(Members);
+	}, [Members]);
+
+	return <Layout name={'Department'}></Layout>;
 }
 
 export default Department;
