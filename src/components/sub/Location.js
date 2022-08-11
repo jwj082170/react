3543073e25953476b52163841892a33b48209762
tubi55 <tubi55@nate.com>
2022-08-11 11:40:35 +0900

    위치 정보값을 반복돌며 버튼 자동 생성

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -32,7 +32,7 @@ function Location() {
 	const container = useRef(null);
 	const [Location, setLocation] = useState(null);
 	const [Traffic, setTraffic] = useState(false);
-	const [Info, setInfo] = useState(info);
+	const [Info] = useState(info);
 	//지점버튼 클릭시 변경되는 순서값이 저장될 스테이트 추가
 	const [Index, setIndex] = useState(0);
 
@@ -52,8 +52,6 @@ function Location() {
 		image: markerImage,
 	});
 
-	//기존에 의존성 배열이 비어져 있을때에는 그냥 컴포넌트 마운트시 지도가 한번 호출되고 마는 구조
-	//의존성 배열에 Index스테이트를 등록하면 지점버튼을 클릭해서 Index스테이트가 변경될때마다 새로 변경된 Index값 정보를 토대로 지도 재호출
 	useEffect(() => {
 		const map_instance = new kakao.maps.Map(container.current, option);
 		marker.setMap(map_instance);
@@ -72,11 +70,17 @@ function Location() {
 			<div id='map' ref={container}></div>
 			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
 
-			{/* 지점보기 버튼 추가후 버튼 클릭시 해당 버튼의 순서값으로 Index스테이트 변경 */}
 			<ul className='branch'>
-				<li onClick={() => setIndex(0)}>삼성동 코엑스</li>
+				{/* <li onClick={() => setIndex(0)}>삼성동 코엑스</li>
 				<li onClick={() => setIndex(1)}>올림픽 공원</li>
-				<li onClick={() => setIndex(2)}>서울 시청</li>
+				<li onClick={() => setIndex(2)}>서울 시청</li> */}
+				{Info.map((info, idx) => {
+					return (
+						<li key={idx} onClick={() => setIndex(idx)}>
+							{info.title}
+						</li>
+					);
+				})}
 			</ul>
 		</Layout>
 	);
