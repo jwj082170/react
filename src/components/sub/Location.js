912b7c4fd1ed81e071a9dd61de12af24a59817b1
tubi55 <tubi55@nate.com>
2022-08-11 09:48:07 +0900

    마커이미지 변경

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -10,8 +10,15 @@ function Location() {
 		level: 3,
 	};
 
+	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png';
+	const imgSize = new kakao.maps.Size(232, 99);
+	const imgPos = { offset: new kakao.maps.Point(116, 99) };
+
+	const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
+
 	const marker = new kakao.maps.Marker({
 		position: option.center,
+		image: markerImage,
 	});
 
 	useEffect(() => {
